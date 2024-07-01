a7c7f0f9ce996068db3638628992b24a42283483
JeremyRamos1803 <jeremy.ramos@usm.cl>
2024-07-01 13:47:32 -0400

    Merge jeremy into fabian

--- project/api/urls.py
+++ project/api/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 # from .views2.view_image import upload_image
 # from .views2.view_food import algo
 
-from . import views
+from .views2 import seguimiento_view
 from .views2 import view_auth
+
 
 from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
@@ -21,14 +22,13 @@ urlpatterns = [
     path('login/', view_auth.login),
     path('register/', view_auth.register),
     path('api/', include(router.urls)),
-    path('hello/', views.hello),
     path('docs/', include_docs_urls),
     # path('upload/', upload_image, name='upload_image'),
     path('upload_2/', ImageUploadView.as_view()),
-    path('api/weekly_summary/', views.get_weekly_summary, name='weekly_summary'),
-    path('api/monthly_summary/', views.get_monthly_summary, name='monthly_summary'),
-    path('api/yearly_summary/', views.get_yearly_summary, name='yearly_summary'),
-    path('api/weekly_summary_price/', views.get_weekly_summary_price, name='weekly_summary_price'),
-    path('api/monthly_summary_price/', views.get_monthly_summary_price, name='monthly_summary_price'),
-    path('api/yearly_summary_price/', views.get_yearly_summary_price, name='yearly_summary_price'),
+    path('api/weekly_summary/', seguimiento_view.get_weekly_summary, name='weekly_summary'),
+    path('api/monthly_summary/', seguimiento_view.get_monthly_summary, name='monthly_summary'),
+    path('api/yearly_summary/', seguimiento_view.get_yearly_summary, name='yearly_summary'),
+    path('api/weekly_summary_price/', seguimiento_view.get_weekly_summary_price, name='weekly_summary_price'),
+    path('api/monthly_summary_price/', seguimiento_view.get_monthly_summary_price, name='monthly_summary_price'),
+    path('api/yearly_summary_price/', seguimiento_view.get_yearly_summary_price, name='yearly_summary_price'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project/project/settings.py
+++ project/project/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'coreapi',
     'api',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
