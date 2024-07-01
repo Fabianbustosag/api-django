cf3c93823c6be36837079354217b9d57354dc6bc
JeremyRamos1803 <jeremy.ramos@usm.cl>
2024-07-01 13:59:43 -0400

    merge martin into fabian

--- project/api/serializers.py
+++ project/api/serializers.py
@@ -20,12 +20,6 @@ class PublicationSerializer(serializers.ModelSerializer):
         model = Publication
         fields = '__all__'
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password']
-from .models import UserData
-
 class UserDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserData
